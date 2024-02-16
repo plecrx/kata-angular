fb648e74b6fbc52d12d8c5a5dce1db6066d1bc44
Prescilla LECURIEUX <prescilla.lecurieux@gmail.com>
2024-02-16 08:24:53 +0100

    chore: add tailwind to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { RouterLink, RouterOutlet } from "@angular/router";
   standalone: true,
   imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: "./app.component.html",
+  styleUrl: "../styles.css",
 })
 export class AppComponent implements OnInit {
   constructor() {}
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from "@angular/router";
 
-import { HomeComponent } from "../pages/home/home.component";
-import { NotFoundComponent } from "../pages/not-found/not-found.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { NotFoundComponent } from "./pages/not-found/not-found.component";
 
 export const routes: Routes = [
   { path: "", component: HomeComponent },
